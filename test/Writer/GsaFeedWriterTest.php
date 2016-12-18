bcdb79dbd8dd4bfc2bb22e92acfd939db75b7868
Grégoire Paris <postmaster@greg0ire.fr>
2016-12-18 17:50:33 +0100

    Add missing leading backslash

--- test/Writer/GsaFeedWriterTest.php
+++ test/Writer/GsaFeedWriterTest.php
@@ -72,7 +72,7 @@ class GsaFeedWriterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @expectedException RuntimeException
+     * @expectedException \RuntimeException
      */
     public function testNonExistentFolder()
     {
