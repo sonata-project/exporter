c21032de486fc6b2984a384c7e001ee545538dcc
maarekj <josephmaarek@gmail.com>
2018-02-27 10:33:59 +0100

    Add tests for DateTimeInterface management (#156)

---
+++ tests/Source/Fixtures/DoctrineORMQuerySourceIterator.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Exporter\Test\Source\Fixtures;
+
+/**
+ * @author Joseph Maarek <josephmaarek@gmail.com>
+ */
+final class DoctrineORMQuerySourceIterator extends \Exporter\Source\DoctrineORMQuerySourceIterator
+{
+    public function getValue($value)
+    {
+        return parent::getValue($value);
+    }
+}
