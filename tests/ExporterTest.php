49069b33ad7ce442c03da9819e2b34b8b29fdac9
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-01-01 17:45:55 +0100

    Applied fixes from FlintCI

--- tests/ExporterTest.php
+++ tests/ExporterTest.php
@@ -11,13 +11,13 @@
 
 namespace Exporter\Tests;
 
+use PHPUnit\Framework\TestCase;
 use Sonata\Exporter\Exporter;
 use Sonata\Exporter\Source\ArraySourceIterator;
 use Sonata\Exporter\Writer\CsvWriter;
 use Sonata\Exporter\Writer\JsonWriter;
 use Sonata\Exporter\Writer\XlsWriter;
 use Sonata\Exporter\Writer\XmlWriter;
-use PHPUnit\Framework\TestCase;
 
 class ExporterTest extends TestCase
 {
