c4666972430215a511836b1bce30ba71614c24bf
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-01-01 17:44:35 +0100

    Applied fixes from FlintCI

--- aliases/Writer/CsvWriter.php
+++ aliases/Writer/CsvWriter.php
@@ -1,13 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Exporter\Writer;
 
 class_alias(
     '\Sonata\\'.__NAMESPACE__.'\CsvWriter',
     __NAMESPACE__.'\CsvWriter'
 );
-
-use Exporter\Exception\InvalidDataFormatException;
 
 if (false) {
     class CsvWriter extends \Sonata\Exporter\Writer\CsvWriter
