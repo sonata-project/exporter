7153179488534f4d2e8f6f1e505f44ee33b9a5dc
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-08-02 17:03:49 +0200

    Update config (#605)

--- src/Bridge/Symfony/Resources/config/services.php
+++ src/Bridge/Symfony/Resources/config/services.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Sonata\Exporter\Exporter;
 use Sonata\Exporter\Writer\CsvWriter;
@@ -18,47 +20,45 @@ use Sonata\Exporter\Writer\JsonWriter;
 use Sonata\Exporter\Writer\XlsWriter;
 use Sonata\Exporter\Writer\XlsxWriter;
 use Sonata\Exporter\Writer\XmlWriter;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // Use "param" function for creating references to parameters when dropping support for Symfony 5.1
     $services = $containerConfigurator->services();
 
     $services->set('sonata.exporter.writer.csv', CsvWriter::class)
         ->args([
-            '%sonata.exporter.writer.csv.filename%',
-            '%sonata.exporter.writer.csv.delimiter%',
-            '%sonata.exporter.writer.csv.enclosure%',
-            '%sonata.exporter.writer.csv.escape%',
-            '%sonata.exporter.writer.csv.show_headers%',
-            '%sonata.exporter.writer.csv.with_bom%',
+            param('sonata.exporter.writer.csv.filename'),
+            param('sonata.exporter.writer.csv.delimiter'),
+            param('sonata.exporter.writer.csv.enclosure'),
+            param('sonata.exporter.writer.csv.escape'),
+            param('sonata.exporter.writer.csv.show_headers'),
+            param('sonata.exporter.writer.csv.with_bom'),
         ]);
 
     $services->set('sonata.exporter.writer.json', JsonWriter::class)
         ->args([
-            '%sonata.exporter.writer.json.filename%',
+            param('sonata.exporter.writer.json.filename'),
         ]);
 
     $services->set('sonata.exporter.writer.xls', XlsWriter::class)
         ->args([
-            '%sonata.exporter.writer.xls.filename%',
-            '%sonata.exporter.writer.xls.show_headers%',
+            param('sonata.exporter.writer.xls.filename'),
+            param('sonata.exporter.writer.xls.show_headers'),
         ]);
 
     if (class_exists(Spreadsheet::class)) {
         $services->set('sonata.exporter.writer.xlsx', XlsxWriter::class)
             ->args([
-                '%sonata.exporter.writer.xlsx.filename%',
-                '%sonata.exporter.writer.xlsx.show_headers%',
-                '%sonata.exporter.writer.xlsx.show_filters%',
+                param('sonata.exporter.writer.xlsx.filename'),
+                param('sonata.exporter.writer.xlsx.show_headers'),
+                param('sonata.exporter.writer.xlsx.show_filters'),
             ]);
     }
 
     $services->set('sonata.exporter.writer.xml', XmlWriter::class)
         ->args([
-            '%sonata.exporter.writer.xml.filename%',
-            '%sonata.exporter.writer.xml.main_element%',
-            '%sonata.exporter.writer.xml.child_element%',
+            param('sonata.exporter.writer.xml.filename'),
+            param('sonata.exporter.writer.xml.main_element'),
+            param('sonata.exporter.writer.xml.child_element'),
         ]);
 
     $services->set('sonata.exporter.exporter', Exporter::class)
