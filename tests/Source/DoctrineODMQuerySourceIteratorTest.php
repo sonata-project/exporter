adf38712d082f7d8cc0de8732f25796ea61b62d5
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-01-02 17:24:24 +0100

    Fix

--- tests/Source/DoctrineODMQuerySourceIteratorTest.php
+++ tests/Source/DoctrineODMQuerySourceIteratorTest.php
@@ -94,7 +94,7 @@ final class DoctrineODMQuerySourceIteratorTest extends TestCase
         $config->setHydratorNamespace('Hydrators');
         $config->setPersistentCollectionDir($directory);
         $config->setPersistentCollectionNamespace('PersistentCollections');
-        $config->setMetadataDriverImpl($config->newDefaultAnnotationDriver()); // @phpstan-ignore-line
+        $config->setMetadataDriverImpl($config->newDefaultAnnotationDriver());
 
         return $config;
     }
