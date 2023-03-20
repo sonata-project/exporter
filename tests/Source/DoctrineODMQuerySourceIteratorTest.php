1ae2f79299279a7b0e3935bb2c9ff711c40780a8
Jordi <jordism91@gmail.com>
2023-03-20 10:02:06 +0100

    Improve doctrine annotations to attributes

--- tests/Source/DoctrineODMQuerySourceIteratorTest.php
+++ tests/Source/DoctrineODMQuerySourceIteratorTest.php
@@ -15,6 +15,8 @@ namespace Sonata\Exporter\Tests\Source;
 
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AttributeDriver;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AttributeReader;
 use PHPUnit\Framework\TestCase;
 use Sonata\Exporter\Source\DoctrineODMQuerySourceIterator;
 use Sonata\Exporter\Tests\Source\Fixtures\Document;
@@ -94,7 +96,7 @@ final class DoctrineODMQuerySourceIteratorTest extends TestCase
         $config->setHydratorNamespace('Hydrators');
         $config->setPersistentCollectionDir($directory);
         $config->setPersistentCollectionNamespace('PersistentCollections');
-        $config->setMetadataDriverImpl($config->newDefaultAnnotationDriver());
+        $config->setMetadataDriverImpl(new AttributeDriver([], new AttributeReader()));
 
         return $config;
     }
